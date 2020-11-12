0a96b4279be11cafed5bc5356e8003678a64193f
kznts9v-1lya <kuznetsov.ilya.alexandrovich@gmail.com>
2020-11-12 23:41:07 +0300

    All system apps are processes now

--- apps.c
+++ apps.c
@@ -18,52 +18,67 @@
 #include "libc.h"
 
 #define APPS_X(X) \
-	X(retcode) \
-	X(readmem) \
-	X(sysecho) \
-	X(sleep) \
-	X(burn) \
-	X(exec) \
+	X(retcode)    \
+	X(readmem)    \
+	X(sysecho)    \
+	X(sleep)      \
+	X(burn)       \
+	X(exec)
 
-#define DECLARE(X) static int app_ ## X(int, char *[]);
+#define DECLARE(X) static int app_##X(int, char *[]);
 APPS_X(DECLARE)
 #undef DECLARE
 
 static int g_retcode;
 
-static const struct app {
+static const struct app
+{
 	const char *name;
 	int (*fn)(int, char *[]);
 } app_list[] = {
-#define ELEM(X) { # X, app_ ## X },
+#define ELEM(X) {#X, app_##X},
 	APPS_X(ELEM)
 #undef ELEM
 };
 
-struct execargs {
+struct execargs
+{
 	int argc;
-	char* argv[16];
+	char *argv[16];
 	char argvbuf[256];
 };
 
-static int exec(int argc, char *argv[]) {
+static int exec(int argc, char *argv[])
+{
 	const struct app *app = NULL;
-	for (int i = 0; i < ARRAY_SIZE(app_list); ++i) {
-		if (!strcmp(argv[0], app_list[i].name)) {
+	for (int i = 0; i < ARRAY_SIZE(app_list); ++i)
+	{
+		if (!strcmp(argv[0], app_list[i].name))
+		{
 			app = &app_list[i];
 			break;
 		}
 	}
 
-	if (app) {
-		return (g_retcode = app->fn(argc, argv));
+	if (app->fn == app_exec)
+	{
+		return app->fn(argc, argv);
 	}
 
-	if (os_fork()) {
+	if (os_fork())
+	{
 		return 0;
 	}
 
-	os_exec(argv[0], argv);
+	if (app)
+	{
+		g_retcode = os_exec(argv[0], argv, argc, app->fn);
+		return 0;
+	}
+	else
+	{
+		os_exec(argv[0], argv, argc, NULL);
+	}
 
 	printf("Unknown command\n");
 	return (g_retcode = 1);
@@ -78,95 +93,114 @@ static int app_echo(int argc, char *argv[]) {
 }
 #endif
 
-static int app_retcode(int argc, char *argv[]) {
+static int app_retcode(int argc, char *argv[])
+{
 	printf("%d\n", g_retcode);
 	return 0;
 }
 
-static int app_readmem(int argc, char *argv[]) {
+static int app_readmem(int argc, char *argv[])
+{
 	unsigned long ptr;
-	if (1 != sscanf(argv[1], "%lu", &ptr)) {
+	if (1 != sscanf(argv[1], "%lu", &ptr))
+	{
 		printf("cannot parse address\n");
 		return 1;
 	}
 
-	printf("%d\n", *(int*)ptr);
+	printf("%d\n", *(int *)ptr);
 	return 0;
 }
 
-static int app_sysecho(int argc, char *argv[]) {
-	for (int i = 1; i < argc - 1; ++i) {
+static int app_sysecho(int argc, char *argv[])
+{
+	for (int i = 1; i < argc - 1; ++i)
+	{
 		os_print(argv[i], strlen(argv[i]));
 		os_print(" ", 1);
 	}
-	if (1 < argc) {
+	if (1 < argc)
+	{
 		os_print(argv[argc - 1], strlen(argv[argc - 1]));
 		os_print("\n", 1);
 	}
-	return argc - 1;
+	return (g_retcode = argc - 1);
 }
 
-
 static long refstart;
-static long reftime(void) {
+static long reftime(void)
+{
 	struct timespec ts;
 	clock_gettime(CLOCK_MONOTONIC, &ts);
 	return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
 }
-static void print(int id, const char *msg) {
+static void print(int id, const char *msg)
+{
 	printf("app1 id %d %s time %d reference %ld\n",
-			id, msg, sched_gettime(), reftime() - refstart);
+		   id, msg, sched_gettime(), reftime() - refstart);
 	fflush(stdout);
 }
 
-static int app_burn(int argc, char* argv[]) {
+static int app_burn(int argc, char *argv[])
+{
 	int id = atoi(argv[1]);
 	int toburn = atoi(argv[2]);
-	while (1)  {
+	while (1)
+	{
 		print(id, "burn");
-		for (volatile int i = 100000 * toburn; 0 < i; --i) {
+		for (volatile int i = 100000 * toburn; 0 < i; --i)
+		{
 		}
 	}
 }
 
-static int app_sleep(int argc, char* argv[]) {
+static int app_sleep(int argc, char *argv[])
+{
 	int id = atoi(argv[1]);
 	int tosleep = atoi(argv[2]);
-	while (1)  {
+	while (1)
+	{
 		print(id, "sleep");
 		sched_sleep(tosleep);
 	}
 }
 
-static int app_exec(int argc, char *argv[]) {
-	os_exec(argv[1], argv + 1);
+static int app_exec(int argc, char *argv[])
+{
+	os_exec(argv[1], argv + 1, argc, NULL);
 	return 0;
 }
 
-static int shell(int argc, char* argv[]) {
+static int shell(int argc, char *argv[])
+{
 	char line[256];
-	while (fgets(line, sizeof(line), stdin)) {
+	while (fgets(line, sizeof(line), stdin))
+	{
 		const char *comsep = "\n;";
 		char *stcmd;
 		char *cmd = strtok_r(line, comsep, &stcmd);
-		while (cmd) {
+		while (cmd)
+		{
 			const char *argsep = " \t";
 			char *starg;
 			char *arg = strtok_r(cmd, argsep, &starg);
 
-			if (arg[0] == '#') {
+			if (arg[0] == '#')
+			{
 				break;
 			}
 
 			char *argv[256];
 			int argc = 0;
-			while (arg) {
+			while (arg)
+			{
 				argv[argc++] = arg;
 				arg = strtok_r(NULL, argsep, &starg);
 			}
 			argv[argc] = NULL;
 
-			if (!argc) {
+			if (!argc)
+			{
 				break;
 			}
 
@@ -177,7 +211,8 @@ static int shell(int argc, char* argv[]) {
 	}
 }
 
-int init(int argc, char* argv[]) {
+int init(int argc, char *argv[])
+{
 	refstart = reftime();
 	shell(argc, argv);
 }
--- sched.c
+++ sched.c
@@ -24,19 +24,22 @@ The 128-byte area beyond the location pointed to by %rsp is considered to
 be reserved and shall not be modified by signal or interrupt handlers */
 #define SYSV_REDST_SZ 128
 
-extern int init(int argc, char* argv[]);
+extern int init(int argc, char *argv[]);
 extern void exittramp(void);
 
 static void timer_bottom(struct hctx *hctx);
 
-struct task {
+struct task
+{
 	char stack[8192];
 
 	struct vmctx vmctx;
-	union {
+	union
+	{
 		struct ctx ctx;
-		struct {
-			int(*main)(int, char**);
+		struct
+		{
+			int (*main)(int, char **);
 			int argc;
 			char **argv;
 		};
@@ -65,34 +68,41 @@ static int taskpool_n;
 
 static sigset_t irqs;
 
-void irq_disable(void) {
+void irq_disable(void)
+{
 	sigprocmask(SIG_BLOCK, &irqs, NULL);
 }
 
-void irq_enable(void) {
+void irq_enable(void)
+{
 	sigprocmask(SIG_UNBLOCK, &irqs, NULL);
 }
 
-static int prio_cmp(struct task *t1, struct task *t2) {
+static int prio_cmp(struct task *t1, struct task *t2)
+{
 	return t1->priority - t2->priority;
 }
 
-static void policy_run(struct task *t) {
+static void policy_run(struct task *t)
+{
 	struct task **c = &runq;
 
-	while (*c && prio_cmp(*c, t) <= 0) {
+	while (*c && prio_cmp(*c, t) <= 0)
+	{
 		c = &(*c)->next;
 	}
 	t->next = *c;
 	*c = t;
 }
 
-static void hctx_push(greg_t *regs, unsigned long val) {
+static void hctx_push(greg_t *regs, unsigned long val)
+{
 	regs[REG_RSP] -= sizeof(unsigned long);
-	*(unsigned long *) regs[REG_RSP] = val;
+	*(unsigned long *)regs[REG_RSP] = val;
 }
 
-static void hctx_call(greg_t *regs, void (*bottom)(struct hctx*)) {
+static void hctx_call(greg_t *regs, void (*bottom)(struct hctx *))
+{
 	unsigned long *savearea = (unsigned long *)(regs[REG_RSP] - SYSV_REDST_SZ);
 
 	*--savearea = regs[REG_RIP];
@@ -119,28 +129,30 @@ static void hctx_call(greg_t *regs, void (*bottom)(struct hctx*)) {
 	regs[REG_RIP] = (unsigned long)bottom;
 }
 
-static void alrmtop(int sig, siginfo_t *info, void *ctx) {
-	ucontext_t *uc = (ucontext_t *) ctx;
+static void alrmtop(int sig, siginfo_t *info, void *ctx)
+{
+	ucontext_t *uc = (ucontext_t *)ctx;
 	greg_t *regs = uc->uc_mcontext.gregs;
 	hctx_call(regs, timer_bottom);
 }
 
-struct task *sched_current(void) {
+struct task *sched_current(void)
+{
 	return current;
 }
 
-int sched_gettime(void) {
+int sched_gettime(void)
+{
 	int cnt1 = timer_cnt();
 	int time1 = time;
 	int cnt2 = timer_cnt();
 	int time2 = time;
 
-	return (cnt1 <= cnt2) ?
-		time1 + cnt2 :
-		time2 + cnt2;
+	return (cnt1 <= cnt2) ? time1 + cnt2 : time2 + cnt2;
 }
 
-static void doswitch(void) {
+static void doswitch(void)
+{
 	struct task *old = current;
 	current = runq;
 	runq = current->next;
@@ -151,25 +163,46 @@ static void doswitch(void) {
 	vmapplymap(&current->vmctx);
 }
 
-static void exectramp(void) {
+static void exectramp(void)
+{
 	irq_enable();
 	current->main(current->argc, current->argv);
 	irq_disable();
 	doswitch();
 }
 
-int sys_exec(struct hctx *hctx, const char *path, char *const argv[]) {
+int sys_exec(struct hctx *hctx, const char *path, char *const argv[], int argc, fn func)
+{
+	if (func)
+	{
+		irq_enable();
+
+		unsigned long result = func(argc, argv);
+		doswitch();
+
+		irq_disable();
+
+		hctx->rax = result;
+
+		return result;
+	}
+
 	char elfpath[32];
 	snprintf(elfpath, sizeof(elfpath), "%s.app", path);
 	int fd = open(elfpath, O_RDONLY);
-	if (fd < 0) {
+	if (fd < 0)
+	{
 		perror("open");
 		return 1;
 	}
 
 	void *rawelf = mmap(NULL, 128 * 1024, PROT_READ, MAP_PRIVATE, fd, 0);
 
-	if (strncmp(rawelf, "\x7f" "ELF" "\x2", 5)) {
+	if (strncmp(rawelf, "\x7f"
+						"ELF"
+						"\x2",
+				5))
+	{
 		printf("ELF header mismatch\n");
 		return 1;
 	}
@@ -183,63 +216,74 @@ int sys_exec(struct hctx *hctx, const char *path, char *const argv[]) {
 	// (we compile loadable apps such way they can be loaded at arbitrary
 	// address)
 
-	const Elf64_Ehdr *ehdr = (const Elf64_Ehdr *) rawelf;
+	const Elf64_Ehdr *ehdr = (const Elf64_Ehdr *)rawelf;
 	if (!ehdr->e_phoff ||
-			!ehdr->e_phnum ||
-			!ehdr->e_entry ||
-			ehdr->e_phentsize != sizeof(Elf64_Phdr)) {
+		!ehdr->e_phnum ||
+		!ehdr->e_entry ||
+		ehdr->e_phentsize != sizeof(Elf64_Phdr))
+	{
 		printf("bad ehdr\n");
 		return 1;
 	}
-	const Elf64_Phdr *phdrs = (const Elf64_Phdr *) (rawelf + ehdr->e_phoff);
+	const Elf64_Phdr *phdrs = (const Elf64_Phdr *)(rawelf + ehdr->e_phoff);
 
 	void *maxaddr = USERSPACE_START;
-	for (int i = 0; i < ehdr->e_phnum; ++i) {
+	for (int i = 0; i < ehdr->e_phnum; ++i)
+	{
 		const Elf64_Phdr *ph = phdrs + i;
-		if (ph->p_type != PT_LOAD) {
+		if (ph->p_type != PT_LOAD)
+		{
 			continue;
 		}
-		if (ph->p_vaddr < IUSERSPACE_START) {
+		if (ph->p_vaddr < IUSERSPACE_START)
+		{
 			printf("bad section\n");
 			return 1;
 		}
-		void *phend = (void*)(ph->p_vaddr + ph->p_memsz);
-		if (maxaddr < phend) {
+		void *phend = (void *)(ph->p_vaddr + ph->p_memsz);
+		if (maxaddr < phend)
+		{
 			maxaddr = phend;
 		}
 	}
 
 	char **copyargv = USERSPACE_START + MAX_USER_MEM - VM_PAGESIZE;
-	char *copybuf = (char*)(copyargv + 32);
+	char *copybuf = (char *)(copyargv + 32);
 	char *const *arg = argv;
 	char **copyarg = copyargv;
-	while (*arg) {
+	while (*arg)
+	{
 		*copyarg++ = strcpy(copybuf, *arg++);
 		copybuf += strlen(copybuf) + 1;
 	}
 	*copyarg = NULL;
 
-	if (vmbrk(&current->vmctx, maxaddr)) {
+	if (vmbrk(&current->vmctx, maxaddr))
+	{
 		printf("vmbrk fail\n");
 		return 1;
 	}
 	vmmakestack(&current->vmctx);
 	vmapplymap(&current->vmctx);
 
-	if (vmprotect(USERSPACE_START, maxaddr - USERSPACE_START, VM_READ | VM_WRITE)) {
+	if (vmprotect(USERSPACE_START, maxaddr - USERSPACE_START, VM_READ | VM_WRITE))
+	{
 		printf("vmprotect RW failed\n");
 		return 1;
 	}
-	for (int i = 0; i < ehdr->e_phnum; ++i) {
+	for (int i = 0; i < ehdr->e_phnum; ++i)
+	{
 		const Elf64_Phdr *ph = phdrs + i;
-		if (ph->p_type != PT_LOAD) {
+		if (ph->p_type != PT_LOAD)
+		{
 			continue;
 		}
-		memcpy((void*)ph->p_vaddr, rawelf + ph->p_offset, ph->p_filesz);
-		int prot = (ph->p_flags & PF_X ? VM_EXEC  : 0) |
-			   (ph->p_flags & PF_W ? VM_WRITE : 0) |
-			   (ph->p_flags & PF_R ? VM_READ  : 0);
-		if (vmprotect((void*)ph->p_vaddr, ph->p_memsz, prot)) {
+		memcpy((void *)ph->p_vaddr, rawelf + ph->p_offset, ph->p_filesz);
+		int prot = (ph->p_flags & PF_X ? VM_EXEC : 0) |
+				   (ph->p_flags & PF_W ? VM_WRITE : 0) |
+				   (ph->p_flags & PF_R ? VM_READ : 0);
+		if (vmprotect((void *)ph->p_vaddr, ph->p_memsz, prot))
+		{
 			printf("vmprotect section failed\n");
 			return 1;
 		}
@@ -247,39 +291,45 @@ int sys_exec(struct hctx *hctx, const char *path, char *const argv[]) {
 
 	struct ctx dummy;
 	struct ctx new;
-	ctx_make(&new, exectramp, (char*)copyargv - 16);
+	ctx_make(&new, exectramp, (char *)copyargv - 16);
 
 	irq_disable();
-	current->main = (void*)ehdr->e_entry;
+	current->main = (void *)ehdr->e_entry;
 	current->argv = copyargv;
 	current->argc = copyarg - copyargv;
 	ctx_switch(&dummy, &new);
 }
 
-static void forktramp(void) {
+static void forktramp(void)
+{
 	vmapplymap(&current->vmctx);
 	exittramp();
 }
 
-int sys_fork(struct hctx *hctx) {
+int sys_fork(struct hctx *hctx)
+{
 	struct task *t = &taskpool[taskpool_n++];
 	hctx->rax = 0;
 	vmctx_copy(&t->vmctx, &current->vmctx);
 	ctx_make(&t->ctx, forktramp, t->stack + sizeof(t->stack) - 16);
+	t->ctx.rbx = (unsigned long)hctx;
 	policy_run(t);
 	return t - taskpool;
 }
 
-static void timer_bottom(struct hctx *hctx) {
+static void timer_bottom(struct hctx *hctx)
+{
 	time += tick_period;
 
-	while (waitq && waitq->waketime <= sched_gettime()) {
+	while (waitq && waitq->waketime <= sched_gettime())
+	{
 		struct task *t = waitq;
 		waitq = waitq->next;
 		policy_run(t);
 	}
 
-	if (tick_period <= sched_gettime() - current_start) {
+	if (tick_period <= sched_gettime() - current_start)
+	{
 		irq_disable();
 		policy_run(current);
 		doswitch();
@@ -287,7 +337,8 @@ static void timer_bottom(struct hctx *hctx) {
 	}
 }
 
-void sched_sleep(unsigned ms) {
+void sched_sleep(unsigned ms)
+{
 
 #if 0
 	if (!ms) {
@@ -302,10 +353,12 @@ void sched_sleep(unsigned ms) {
 	current->waketime = sched_gettime() + ms;
 
 	int curtime;
-	while ((curtime = sched_gettime()) < current->waketime) {
+	while ((curtime = sched_gettime()) < current->waketime)
+	{
 		irq_disable();
 		struct task **c = &waitq;
-		while (*c && (*c)->waketime < current->waketime) {
+		while (*c && (*c)->waketime < current->waketime)
+		{
 			c = &(*c)->next;
 		}
 		current->next = *c;
@@ -316,7 +369,8 @@ void sched_sleep(unsigned ms) {
 	}
 }
 
-static void inittramp2(void) {
+static void inittramp2(void)
+{
 	irq_enable();
 
 	init(0, NULL);
@@ -325,7 +379,8 @@ static void inittramp2(void) {
 	doswitch();
 }
 
-static void inittramp(void) {
+static void inittramp(void)
+{
 	vmmakestack(&current->vmctx);
 	vmapplymap(&current->vmctx);
 
@@ -335,7 +390,8 @@ static void inittramp(void) {
 	ctx_switch(&dummy, &new);
 }
 
-static void sched_run(int period_ms) {
+static void sched_run(int period_ms)
+{
 	sigemptyset(&irqs);
 	sigaddset(&irqs, SIGALRM);
 
@@ -354,16 +410,20 @@ static void sched_run(int period_ms) {
 	}
 
 	tick_period = period_ms;
-	/*timer_init_period(period_ms, alrmtop);*/
+	timer_init_period(period_ms, alrmtop);
 
 	current = &idle;
 	vmctx_make(&current->vmctx);
 
-	while (runq || waitq) {
-		if (runq) {
+	while (runq || waitq)
+	{
+		if (runq)
+		{
 			policy_run(current);
 			doswitch();
-		} else {
+		}
+		else
+		{
 			sigsuspend(&none);
 		}
 	}
@@ -371,12 +431,14 @@ static void sched_run(int period_ms) {
 	irq_enable();
 }
 
-static void segvtop(int sig, siginfo_t *info, void *ctx) {
-	ucontext_t *uc = (ucontext_t *) ctx;
+static void segvtop(int sig, siginfo_t *info, void *ctx)
+{
+	ucontext_t *uc = (ucontext_t *)ctx;
 	greg_t *regs = uc->uc_mcontext.gregs;
 
 	uint16_t *ins = (uint16_t *)regs[REG_RIP];
-	if (*ins != 0x81cd) {
+	if (*ins != 0x81cd)
+	{
 		abort();
 	}
 
@@ -385,14 +447,16 @@ static void segvtop(int sig, siginfo_t *info, void *ctx) {
 	hctx_call(regs, syscall_bottom);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	struct sigaction act = {
 		.sa_sigaction = segvtop,
 		.sa_flags = SA_RESTART,
 	};
 	sigemptyset(&act.sa_mask);
 
-	if (-1 == sigaction(SIGSEGV, &act, NULL)) {
+	if (-1 == sigaction(SIGSEGV, &act, NULL))
+	{
 		perror("signal set failed");
 		return 1;
 	}
